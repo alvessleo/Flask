c71544f2b9a33117f5e9715e2617f979db3a5f52
Leonardo Alves <alvesleo2002@gmail.com>
2023-01-25 20:34:54 -0300

    CRUD - DELETE

--- PhoneList/crudTodo.py
+++ PhoneList/crudTodo.py
@@ -1,8 +1,7 @@
 from flask import Flask, jsonify, request, render_template, Response
 app = Flask(__name__)
 
-contacts = [{'id': 1, 'name': 'John Doe', 
-'phone': '555-555-5555'}]
+contacts = [{'id': 1, 'name': 'John Doe', 'phone': '555-555-5555'}]
 
 @app.route('/contactlist',methods=['GET'])
 def contactlist():
@@ -11,7 +10,9 @@ def contactlist():
 # GET request to retrieve all contacts
 @app.route('/contacts', methods=['GET'])
 def get_contacts():
-    return {'contacts': contacts}
+    if not contacts:
+        return jsonify({'message': 'No contacts found in server'}), 404
+    return jsonify({'contacts': contacts}), 200
 
 # GET request to retrieve one contacts
 @app.route('/contacts/<int:id>', methods=['get'])
@@ -25,6 +26,15 @@ def get_contact(id):
 # POST request to add a new contact with data of the new contact on a json file
 @app.route('/contacts', methods=['POST'])
 def add_contact():
+    if not request.is_json:
+        return jsonify({'message': 'body is not a json'}), 415
+    data = request.get_json()
+    if not data or not all(key in data for key in ('name', 'phone')):
+        return jsonify({'message': 'bad request'}), 400
+
+    # id = 1
+    # if len(contact) > 0:
+    #     id = contact[-1]['id']+1    
     #id is created here 
     name = request.json['name']
     phone = request.json['phone']
@@ -38,7 +48,7 @@ def add_contact():
     print("CCC", contact['name'], contact['phone'])
     print("DDD", contact)
     contacts.append(contact)
-    return {'contact': contact}
+    return jsonify({'contact': contact}), 201
 
 # PUT request to update a contact
 @app.route('/contacts/<int:id>', methods=['PUT'])
@@ -52,19 +62,20 @@ def update_contact(id):
     print("phone ", phone)
     for c in contacts:
         if id == c['id']:
-            if request.json['name']:
-                c['name'] = request.json['name']
-            elif request.json['phone']:
-                c['phone'] = request.json['phone']
-        updated_contact = c
-        
-        return {'contact': updated_contact}
+            c['name'] = request.json['name']
+            c['phone'] = request.json['phone']
+            updated_contact = c
+            return {'contact': updated_contact}
     return Response("Contato inexistente", status=400)
 
 # DELETE request to delete a contact
 @app.route('/contacts/<int:id>', methods=['DELETE'])
 def delete_contact(id):
-    return {'message': ''}
+    for i,contact in enumerate(contacts):
+        if id == contact["id"]:
+            del contacts[i]
+            return jsonify({'contact': contact})
+    return {'message': 'Deleted'}
 
 
 app.run(debug=True,port = 5001)
